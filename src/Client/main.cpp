727fb485565e110df275647429b0a54c24fafb34
Elis Odenhage <teknikgillaren@gmail.com>
2022-08-22 12:45:51 +0200

    print out board

--- src/Client/main.cpp
+++ src/Client/main.cpp
@@ -1,5 +1,25 @@
-#include <stdio.h>
+#include <iostream>
+#include <string>
+
+class GameData{
+public:
+  bool done;
+  char board[9] = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+  std::string in;
+};
 
 int main(int argc, char *argv[]){
-  printf("Hello, World!");
+  GameData      gd;
+
+  gd.done = false;
+  while(!gd.done){
+    for(int i=0;i<sizeof(gd.board);i++){
+      std::cout << gd.board[i];
+      if(i==2||i==5||i==8){
+	std::cout << "\n";
+      }
+    }
+    std::cin >> gd.in;
+  }
+  return 0;
 }

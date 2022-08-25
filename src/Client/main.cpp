98ec049332c65f01ad6639f46103a921bb97b43c
Elis Odenhage <teknikgillaren@gmail.com>
2022-08-25 13:44:40 +0200

    make move online

--- src/Client/main.cpp
+++ src/Client/main.cpp
@@ -1,26 +1,39 @@
+#include <arpa/inet.h>
 #include <iostream>
 #include <stdio.h>
+#include <string.h>
 #include <string>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
 
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <iostream>
+#define PORT 8080
+  
 class GameData {
 public:
   bool done;
   char board[9] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
-  char winBoards[8][9] = {{'X', ' ', ' ', ' ', 'X', ' ', ' ', ' ', 'X'},
-                          {' ', ' ', 'X', ' ', 'X', ' ', 'X', ' ', ' '},
-                          {'X', 'X', 'X', ' ', ' ', ' ', ' ', ' ', ' '},
-                          {' ', ' ', ' ', 'X', 'X', 'X', ' ', ' ', ' '},
-                          {' ', ' ', ' ', ' ', ' ', ' ', 'X', 'X', 'X'},
-                          {'X', ' ', ' ', 'X', ' ', ' ', 'X', ' ', ' '},
-                          {' ', 'X', ' ', ' ', 'X', ' ', ' ', 'X', ' '},
-                          {' ', ' ', 'X', ' ', ' ', 'X', ' ', ' ', 'X'}
-
-  };
-  std::string in;
+  char in[1];
   char turn;
 };
 
+class ClientData {
+public:
+  int sock = 0, valread, client_fd;
+  struct sockaddr_in serv_addr;
+  char buffer[1024] = {0};
+};
+
 GameData gd;
+ClientData cd;
 
 int printBoard() {
   for (int i = 0; i < sizeof(gd.board); i++) {
@@ -32,29 +45,52 @@ int printBoard() {
   return 0;
 }
 
-int switchTurn() {
-  if (gd.turn == 'X') {
-    gd.turn = 'O';
-  } else {
-    gd.turn = 'X';
-  }
-  return 0;
-}
-int makeMove() {
-  if (gd.board[std::stoi(gd.in)] != ' ') {
-    return 1;
+int client() {
+  if ((cd.sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    printf("\n Socket creation error \n");
+    return -1;
   }
 
-  gd.board[std::stoi(gd.in)] = gd.turn;
+  cd.serv_addr.sin_family = AF_INET;
+  cd.serv_addr.sin_port = htons(PORT);
+
+  // Convert IPv4 and IPv6 addresses from text to binary
+  // form
+  if (inet_pton(AF_INET, "127.0.0.1", &cd.serv_addr.sin_addr) <= 0) {
+    printf("\nInvalid address/ Address not supported \n");
+    return -1;
+  }
+
+  if ((cd.client_fd = connect(cd.sock, (struct sockaddr *)&cd.serv_addr,
+                           sizeof(cd.serv_addr))) < 0) {
+    printf("\nConnection Failed \n");
+    return -1;
+  }
+
   return 0;
+}
+
+int write(const char* message){
+  send(cd.sock, message, sizeof(message), 0);
+  printf("Hello message sent\n");
+}
+
+
+int read(){
+  cd.valread = read(cd.sock, cd.buffer, 1024);
+  printf("%s\n", cd.buffer);
+}
+
+int makeMove(){
+  std::string m = std::string("[MOVE] ") + std::string(gd.in);
+  write(m.c_str());
 }
 
 int move() {
   bool invalidMove = true;
   while (invalidMove) {
-    std::cout << "It's your turn " << gd.turn << ": ";
-    std::cin >> gd.in;
-    gd.in = (std::to_string(((std::stoi(gd.in)) - 1)));
+    std::cout << "It's your turn: ";
+    std::cin >> gd.in[0];
     int err = makeMove();
     if (err != 0) {
       std::cout << "Error while making move: " << err << "\n";
@@ -68,69 +104,19 @@ int move() {
   }
   return 0;
 }
-
-int checkWin() {
-  if (gd.board[0] == gd.turn && gd.board[1] == gd.turn &&
-          gd.board[2] == gd.turn ||
-      gd.board[3] == gd.turn && gd.board[4] == gd.turn &&
-          gd.board[5] == gd.turn ||
-      gd.board[6] == gd.turn && gd.board[7] == gd.turn &&
-          gd.board[8] == gd.turn ||
-      gd.board[0] == gd.turn && gd.board[3] == gd.turn &&
-          gd.board[6] == gd.turn ||
-      gd.board[1] == gd.turn && gd.board[4] == gd.turn &&
-          gd.board[7] == gd.turn ||
-      gd.board[2] == gd.turn && gd.board[5] == gd.turn &&
-          gd.board[8] == gd.turn ||
-      gd.board[0] == gd.turn && gd.board[4] == gd.turn &&
-          gd.board[8] == gd.turn ||
-      gd.board[2] == gd.turn && gd.board[4] == gd.turn &&
-          gd.board[6] == gd.turn) {
-    return 0;
-  }
-  return 1;
-}
-
-int checkTie(){
-  int isTie = 0;
-  for(int i=0;i<sizeof(gd.board);i++){
-    if(gd.board[i]!=' '){
-      isTie++;
-    }
-  }
-  if(isTie==9){
-    return 0;
-  }
-  return 1;
-}
-
-int won() {
-  std::cout << gd.turn << " won!\n";
-  return 0;
-}
-
-int tie() {
-  std::cout << "It's a tie!\n";
-  return 0;
-}
 int main(int argc, char *argv[]) {
-  bool invalidMove = true;
+
+  // client
+  client();
+
   gd.done = false;
-  gd.turn = 'X';
-  while (!gd.done) {
-    printBoard();
-    move();
-    int err = checkWin();
-    if (err == 0) {
-      won();
-      return 0;
-    }
-    err = checkTie();
-    if(err==0){
-      tie();
-      return 0;
-    }
-    switchTurn();
-  }
+  // while (!gd.done) {
+  //   printBoard();
+  //   move();
+  // }
+  write("test");
+  read();
+  // closing the connected socket
+  close(cd.client_fd);
   return 0;
 }
